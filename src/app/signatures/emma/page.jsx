8f3ebbd296f171b2acacba9ff67075d8a27e6977
Olivier Carignan <olive.carignan@gmail.com>
2026-06-25 11:10:37 -0400

    upload assets

---
+++ src/app/signatures/emma/page.jsx
@@ -0,0 +1,84 @@
+export default function Emma() {
+  return (
+    <main className="subgrid signature">
+      <section className="signature">
+        <div
+          dangerouslySetInnerHTML={{
+            __html: `
+            <html lang="en">
+                <head>
+                  <title>Emma Wismer email signature</title>
+                </head>
+                <body>
+                  <table
+                    width="100%"
+                    border="0"
+                    cellpadding="0"
+                    cellspacing="0"
+                    style="
+                    font-family: Helvetica Neue, system-ui, sans-serif;
+                    font-size: 16px;
+                    line-height: 1.08;
+                    font-weight: 500;
+                    text-transform: uppercase;
+                    letter-spacing: 0em;
+                  "
+                  >
+                    <tr>
+                      <td>
+                        <p style="font-size: 16px; margin: 0; padding-top: 16px; color: #000">
+                          Emma Wismer
+                        </p>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <p
+                          style="
+                          padding-bottom: 12px;
+                          margin: 0;
+                          font-size: 16px;
+                          color: #000;
+                        "
+                        >
+                        Founder & Principal
+                        </p>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <p style="font-size: 16px; margin: 0; padding-bottom: 12px; color: #000; text-decoration: none !important;">
+                          <a
+                            href="tel:+15144029382"
+                            style="
+                            margin: 0;
+                            font-size: 16px;
+                            color: #000;
+                          "
+                          >
+                          +1 514-402-9382
+                          </a>
+                        </p>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <a href="https://colophon.online">
+                          <img
+                            src="https://colophon.online/images/png/colophon_logotype.png"
+                            alt="colophon logo"
+                            style="width: 16px; height: 18px; object-fit: contain;"
+                          />
+                        </a>
+                      </td>
+                    </tr>
+                  </table>
+                </body>
+              </html>
+            `,
+          }}
+        />
+      </section>
+    </main>
+  );
+}
